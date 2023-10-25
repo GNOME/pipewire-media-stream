036498306ec48cc31f0235fdfc72e6802fb6a9d8
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2023-10-25 12:18:07 -0300

    main: Switch to AdwApplication

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
+#include <adwaita.h>
 #include <glib/gi18n.h>
 #include <pipewire/pipewire.h>
 
 #include "window.h"
-
-static void
-on_startup (GtkApplication *app)
-{
-  (void) app;
-
-  adw_init ();
-}
 
 static void
 on_activate (GtkApplication *app)
@@ -30,12 +23,11 @@ int
 main (int   argc,
       char *argv[])
 {
-	g_autoptr(GtkApplication) app = NULL;
+  g_autoptr(AdwApplication) app = NULL;
 
   pw_init (&argc, &argv);
 
-  app = gtk_application_new ("com.feaneron.example.PipeWireMediaStream", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "startup", G_CALLBACK (on_startup), NULL);
+  app = adw_application_new ("com.feaneron.example.PipeWireMediaStream", G_APPLICATION_DEFAULT_FLAGS);
 	g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL);
 
 	return g_application_run (G_APPLICATION (app), argc, argv);
