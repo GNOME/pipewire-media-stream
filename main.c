e98312d91bd538224983c09c820b61c0d695c994
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2023-10-25 12:18:27 -0300

    main: Remove tabs

--- main.c
+++ main.c
@@ -9,14 +9,14 @@ on_activate (GtkApplication *app)
 {
   static GtkWindow *window = NULL;
 
-	if (!window)
+  if (!window)
     {
       window = g_object_new (PMS_TYPE_WINDOW,
                              "application", app,
                              NULL);
     }
 
-	gtk_window_present (window);
+  gtk_window_present (window);
 }
 
 int
@@ -28,7 +28,7 @@ main (int   argc,
   pw_init (&argc, &argv);
 
   app = adw_application_new ("com.feaneron.example.PipeWireMediaStream", G_APPLICATION_DEFAULT_FLAGS);
-	g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL);
+  g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL);
 
-	return g_application_run (G_APPLICATION (app), argc, argv);
+  return g_application_run (G_APPLICATION (app), argc, argv);
 }
